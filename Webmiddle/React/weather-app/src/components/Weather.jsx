1b5e2a53eecf24b663e655a66a04f0208215c1d2
Fazdagam628 <fauzanmasud628@gmail.com>
2025-03-20 11:27:31 +0700

    asd

--- Webmiddle/React/weather-app/src/components/Weather.jsx
+++ Webmiddle/React/weather-app/src/components/Weather.jsx
@@ -1,14 +1,119 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./Weather.css";
+import clear_icon from "../assets/clear.png";
+import clouds_icon from "../assets/clouds.png";
+import drizzle_icon from "../assets/drizzle.png";
+import fog_icon from "../assets/fog.png";
+import haze_icon from "../assets/haze.png";
+import humidity_icon from "../assets/humidity.png";
+import mist_icon from "../assets/mist.png";
+import rain_icon from "../assets/rain.png";
 import search_icon from "../assets/search.png";
+import snow_icon from "../assets/snow.png";
+import wind_icon from "../assets/wind.png";
 
 const Weather = () => {
+  const inputRef = useRef();
+  const [weatherData, setWeatherData] = useState(false);
+
+  const allIcons = {
+    "01d": "https://openweathermap.org/img/wn/01d@2x.png",
+    "01n": "https://openweathermap.org/img/wn/01n@2x.png",
+    "02d": "https://openweathermap.org/img/wn/02d@2x.png",
+    "02n": "https://openweathermap.org/img/wn/02n@2x.png",
+    "03d": "https://openweathermap.org/img/wn/03d@2x.png",
+    "03n": "https://openweathermap.org/img/wn/03n@2x.png",
+    "04d": "https://openweathermap.org/img/wn/04d@2x.png",
+    "04n": "https://openweathermap.org/img/wn/04n@2x.png",
+    "09d": "https://openweathermap.org/img/wn/09d@2x.png",
+    "09n": "https://openweathermap.org/img/wn/09n@2x.png",
+    "10d": "https://openweathermap.org/img/wn/10d@2x.png",
+    "10n": "https://openweathermap.org/img/wn/10n@2x.png",
+    "11d": "https://openweathermap.org/img/wn/11d@2x.png",
+    "11n": "https://openweathermap.org/img/wn/11n@2x.png",
+    "13d": "https://openweathermap.org/img/wn/13d@2x.png",
+    "13n": "https://openweathermap.org/img/wn/13n@2x.png",
+    "50d": "https://openweathermap.org/img/wn/50d@2x.png",
+    "50n": "https://openweathermap.org/img/wn/50n@2x.png",
+  };
+  const search = async (city) => {
+    if (city === "") {
+      alert("Masukkan nama kota terlebih dahulu");
+      return;
+    }
+    try {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${
+        import.meta.env.VITE_APP_ID
+      }`;
+
+      const response = await fetch(url);
+      const data = await response.json();
+
+      if (!response.ok) {
+        alert(data.message);
+        return;
+      }
+
+      console.log("====================================");
+      console.log(data);
+      console.log("====================================");
+      const icon = allIcons[data.weather[0].icon] || clear_icon;
+      setWeatherData({
+        humidity: data.main.humidity,
+        windSpeed: data.wind.speed,
+        temperature: Math.floor(data.main.temp),
+        location: data.name,
+        icon: icon,
+      });
+    } catch (error) {
+      setWeatherData(false);
+      console.error("Error in fetching weather data");
+    }
+  };
+
+  useEffect(() => {
+    search("Semarang");
+  }, []);
+
   return (
     <div className="weather">
       <div className="search-bar">
-        <input type="text" placeholder="Cari" />
-        <img src={search_icon} alt="" />
+        <input ref={inputRef} type="text" placeholder="Cari" />
+        <img
+          src={search_icon}
+          alt="Search Icon"
+          onClick={() => search(inputRef.current.value)}
+        />
       </div>
+      {weatherData ? (
+        <>
+          <img
+            src={weatherData.icon}
+            alt="Clear Icon"
+            className="weather-icon"
+          />
+          <p className="temperature">{weatherData.temperature}°c</p>
+          <p className="location">{weatherData.location}</p>
+          <div className="weather-data">
+            <div className="col">
+              <img src={humidity_icon} alt="Humidity Icon" />
+              <div>
+                <p>{weatherData.humidity}</p>
+                <span>Humidity</span>
+              </div>
+            </div>
+            <div className="col">
+              <img src={wind_icon} alt="Wind Icon" />
+              <div>
+                <p>{weatherData.windSpeed} Km/h</p>
+                <span>Wind Speed</span>
+              </div>
+            </div>
+          </div>
+        </>
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
