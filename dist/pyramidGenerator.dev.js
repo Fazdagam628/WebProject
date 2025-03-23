3a8e66a495a44f67c5d8e3d0f5c296b87e798593
Fazdagam628 <fauzanmasud628@gmail.com>
2025-03-23 11:53:20 +0700

    Minor change

---
+++ dist/pyramidGenerator.dev.js
@@ -0,0 +1,27 @@
+"use strict";
+
+var character = "|";
+var count = 12;
+var rows = [];
+var inverted = false;
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+for (var i = 1; i <= count; i++) {
+  if (inverted) {
+    rows.unshift(padRow(i, count));
+  } else {
+    rows.push(padRow(i, count));
+  }
+}
+
+var result = "";
+
+for (var _i = 0, _rows = rows; _i < _rows.length; _i++) {
+  var row = _rows[_i];
+  result = result + row + "\n";
+}
+
+console.log(result);
