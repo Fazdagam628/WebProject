870428cb83c17f81e1b769a5aae31958c0d6d253
Fazdagam628 <fauzanmasud628@gmail.com>
2025-03-31 10:37:04 +0700

    add

--- javascriptLanjutan/6.2/dist/script.dev.js
+++ javascriptLanjutan/6.2/dist/script.dev.js
@@ -1,15 +1,14 @@
+"use strict";
+
 // Destructuring
 // function kalkulasi(a, b) {
 //   return [a + b, a - b, a * b, a / b];
 // }
-
 // const jumlah = penjumlahanPerkalian(2, 3)[0];
 // const kali = penjumlahanPerkalian(2, 3)[1];
-
 // const [jumlah, kali] = penjumlahanPerkalian(2, 3);
 // console.log(jumlah);
 // console.log(kali);
-
 // const [
 //   tambah = "Tidak ada",
 //   kurang = "Tidak ada",
@@ -17,7 +16,6 @@
 //   bagi = "Tidak ada",
 // ] = kalkulasi(2, 3);
 // console.log(bagi);
-
 // function kalkulasi(a, b) {
 //   return {
 //     tambah: a + b,
@@ -29,20 +27,17 @@
 //
 // const { bagi, kali, kurang, tambah } = kalkulasi(4, 5);
 // console.log(kurang);
-
 // Destructuring function arguments
-const mhs1 = {
+var mhs1 = {
   nama: "Fauzan",
   umur: 18,
   email: "Fauzanmasud628@gmail.com",
   nilai: {
     tugas: 80,
     uts: 85,
-    uas: 75,
-  },
-};
-
-// function cetakMhs(nama, umur) {
+    uas: 75
+  }
+}; // function cetakMhs(nama, umur) {
 //   return `Halo, nama saya ${nama},saya berumur ${umur} tahun`;
 // }
 // function cetakMhs(mhs) {
@@ -51,8 +46,14 @@ const mhs1 = {
 //
 // console.log(cetakMhs(mhs1));
 
-function cetakMhs({ nama, umur, nilai: { tugas, uts, uas } }) {
-  return `Halo, nama saya ${nama},saya berumur ${umur} tahun. nilai uas saya ${uas}`;
+function cetakMhs(_ref) {
+  var nama = _ref.nama,
+      umur = _ref.umur,
+      _ref$nilai = _ref.nilai,
+      tugas = _ref$nilai.tugas,
+      uts = _ref$nilai.uts,
+      uas = _ref$nilai.uas;
+  return "Halo, nama saya ".concat(nama, ",saya berumur ").concat(umur, " tahun. nilai uas saya ").concat(uas);
 }
 
 console.log(cetakMhs(mhs1));
