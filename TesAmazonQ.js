dbbb5c30461e23be74a9cc2bf0faee78fbd8d9cc
Fazdagam628 <fauzanmasud628@gmail.com>
2025-05-11 21:02:36 +0700

    add

---
+++ TesAmazonQ.js
@@ -0,0 +1,9 @@
+function square(n) {
+  return n * n;
+}
+
+function calculate(n) {
+  console.log(square(n));
+}
+
+calculate(10);
---
+++ dist/TesAmazonQ.dev.js
@@ -0,0 +1,11 @@
+"use strict";
+
+function square(n) {
+  return n * n;
+}
+
+function calculate(n) {
+  console.log(square(n));
+}
+
+calculate(10);
